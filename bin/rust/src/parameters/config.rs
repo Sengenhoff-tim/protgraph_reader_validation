af083a4ad80a3f446248be49d7f4372a9c191666
Tim Sengenhoff <Tim.Sengenhoff@ruhr-uni-bochum.de>
2026-05-20 13:34:17 +0200

    added  input validation for splits + split depth

--- bin/rust/src/parameters/config.rs
+++ bin/rust/src/parameters/config.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use clap::Parser;
 
 use crate::parameters::Cli;
@@ -16,6 +16,19 @@ pub struct Config {
 impl Config {
     pub fn new() -> Result<Config> {
         let cli = Cli::parse();
+
+        if let Some(result) = cli.job_splits.checked_pow(cli.job_split_depth as u32) {
+            if result as i64 > WEIGHT_FACTOR {
+                bail!(
+                    "Job splits out of bounds. Current: {}^{}; Maximum: {}",
+                    cli.job_splits,
+                    cli.job_split_depth,
+                    WEIGHT_FACTOR
+                )
+            }
+        } else {
+            bail!("Job splits calculation overflowed")
+        }
 
         // queries are read from query input csv in Da
         let intervals = read_query_csv(&cli.query_input_path, WEIGHT_FACTOR)?;
